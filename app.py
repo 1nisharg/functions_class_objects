c0cfce01c306d4521fc129ade83223b16431ed52
1nisharg <80333086+1nisharg@users.noreply.github.com>
2022-09-10 13:11:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from chef import chef
+
+mychef = chef()
+mychef.make_special_dish()
+
+from chinesechef import chinesechef
+
+mychinesechef = chinesechef()
+mychinesechef.make_salad()
---
+++ chinesechef.py
@@ -0,0 +1,11 @@
+class chinesechef:
+     def make_chicken(self):
+        print("the chef makes a chicken")
+     def make_salad(self):
+            print("the chef makes a salad")
+
+     def make_special_dish(self):
+                print("the chef makes bbq")
+
+                def make_rice(self):
+                    print("makes rice")
